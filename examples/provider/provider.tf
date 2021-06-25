b4b735be45a28dc961b366e84a867c7dd1e3d119
Tim Crowder <tim@shoreline.io>
2021-06-25 00:07:02 -0700

    Add examples.

---
+++ examples/provider/provider.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    shoreline = {
+      source  = "shoreline.io/terraform/shoreline"
+      version = ">= 1.0.1"
+    }
+  }
+}
+
+provider "shoreline" {
+  # provider configuration here
+  #token = "xyz1.asdfj.asd3fas..."
+  url = "https://test.us.api.shoreline-vm1.io"
+  #url = "https://test.us.api.shoreline-test6.io"
+  retries = 2
+  debug = true
+}
+
---
+++ examples/resources/action/resource.tf
@@ -0,0 +1,20 @@
+
+resource "shoreline_action" "ls_action" {
+  name = "ls_action"
+  command = "`ls $${dir}; export FOO='bar'`"
+  description = "List some files ..."
+  resource_query = "host"
+  params = [ "dir" ]
+  res_env_var = "FOO"
+  #timeout = 60
+  start_title_template    = "JVM dump started"
+  complete_title_template = "JVM dump completed"
+  error_title_template    = "JVM dump failed"
+
+  start_short_template    = "JVM dump short started"
+  complete_short_template = "JVM dump short completed"
+  error_short_template    = "JVM dump short failed"
+
+  enabled = true
+}
+
