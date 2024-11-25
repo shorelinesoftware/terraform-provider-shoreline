d2df20560859325a9f3c8f25d49b24936e2eba17
amalia101 <amalia@shoreline.io>
2024-11-25 13:01:20 +0200

    add dashboard support

--- provider/provider_conf.go
+++ provider/provider_conf.go
@@ -327,6 +327,19 @@ var ObjectConfigJsonStr = `
        }
     },
 
+	"dashboard": {
+       "attributes": {
+           "type":               { "type": "string",     "computed": true, "value": "DASHBOARD" },
+           "name":               { "type": "label",      "required": true, "forcenew": true, "skip": true},
+           "display_name":       { "type": "string",     "optional": true, "match_null": ""},
+           "dashboard_type":     { "type": "string",     "required": true, "primary": true },
+           "resource_query":     { "type": "string",     "optional": true, "step": "dashboard_configuration.resource_query" },
+           "groups":             { "type": "b64json",    "optional": true, "step": "dashboard_configuration.groups", "outtype": "json" },
+           "values":             { "type": "b64json",    "optional": true, "step": "dashboard_configuration.values", "outtype": "json" },
+           "other_tags":         { "type": "string_set", "optional": true, "step": "dashboard_configuration.other_tags" }
+       }
+    },
+
 	"docs": {
 		"objects": {
 			"action":    "A command that can be run.\n\nSee the Shoreline [Actions Documentation](https://docs.shoreline.io/actions) for more info.",
@@ -461,7 +474,11 @@ var ObjectConfigJsonStr = `
 			"client_secret":           "Client secret for a 3rd-party service integration (Microsoft Entra ID).",
 			"display_name":            "A user friendly name shown in the UI.",
 			"blocks":           	   "The JSON encoded blocks of the report template.",
-			"links":           	   	   "The JSON encoded links of a report template with other report templates."
+			"links":           	   	   "The JSON encoded links of a report template with other report templates.",
+			"dashboard_type":          "Specifies the type of the dashboard configuration. Currently, only 'TAGS_SEQUENCE' is supported.",
+			"groups":                  "A JSON-encoded list of groups in the dashboard configuration. Each group is an object with 'name' (the group's name) and 'tags' (a list of tag names belonging to the group). Tags not assigned to any group must be included in a default group named '__unclassified__'.",
+			"values":                  "A JSON-encoded list of objects defining the values and their associated colors in the dashboard configuration. Each object contains: 'color' (the color associated with the values) and 'values' (a list of values corresponding to specific tags).",
+			"other_tags":              "A list of additional tags that will be displayed for the resources."
 		}
 	}
 }
