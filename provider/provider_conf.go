bbcd3ce4a4157fd93ca75c4e7d949e44572d15e2
amalia101 <amalia@shoreline.io>
2024-12-20 11:22:25 +0200

    remove display name from dashboard

--- provider/provider_conf.go
+++ provider/provider_conf.go
@@ -331,7 +331,6 @@ var ObjectConfigJsonStr = `
        "attributes": {
            "type":               { "type": "string",     "computed": true, "value": "DASHBOARD" },
            "name":               { "type": "label",      "required": true, "forcenew": true, "skip": true},
-           "display_name":       { "type": "string",     "optional": true, "match_null": ""},
            "dashboard_type":     { "type": "string",     "required": true, "primary": true },
            "resource_query":     { "type": "string",     "optional": true, "step": "dashboard_configuration.resource_query" },
            "groups":             { "type": "b64json",    "optional": true, "step": "dashboard_configuration.groups", "outtype": "json" },
@@ -473,7 +472,6 @@ var ObjectConfigJsonStr = `
 			"tenant_id":               "Tenant id for a 3rd-party service integration (Microsoft Entra ID).",
 			"client_id":               "Application id for a 3rd-party service integration (Microsoft Entra ID).",
 			"client_secret":           "Client secret for a 3rd-party service integration (Microsoft Entra ID).",
-			"display_name":            "A user friendly name shown in the UI.",
 			"blocks":           	   "The JSON encoded blocks of the report template.",
 			"links":           	   	   "The JSON encoded links of a report template with other report templates.",
 			"dashboard_type":          "Specifies the type of the dashboard configuration. Currently, only 'TAGS_SEQUENCE' is supported.",
